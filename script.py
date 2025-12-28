7d583b515bd8935627902415f48cafdcb7f8ba8c
ethanderouis <105126976+ethanderouis@users.noreply.github.com>
2025-12-28 05:31:41 -0500

    Added test subjects

---
+++ script.py
@@ -0,0 +1,4 @@
+# SETTING UP YOU PROJECT
+
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ['historical site', 'art']]
